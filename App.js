02c205105994c8af004e26752dc0e55b2200854d
Sitc <aranyi8921@gmail.com>
2020-03-02 17:19:27 +0100

    first

---
+++ App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Let start developing native apps</Text>
+      <Text>Let start developing native apps</Text>
+      <Text>Let start developing native apps</Text>
+      <Text>Let start developing native apps</Text>
+      <Text>Let start developing native apps</Text>
+      <Text>Let start developing native apps</Text>
+      <Text>Let start developing native apps</Text>
+      <Text>Let start developing native apps</Text>
+      <Text>Let start developing native apps</Text>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
